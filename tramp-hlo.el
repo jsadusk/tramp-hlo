2f208390b63e1593e7959206e4bc98a7afd85083
Joe Sadusk <joe@sadusk.com>
2025-10-26 22:01:56 -0700

    Updated header and license

--- tramp-hlo.el
+++ tramp-hlo.el
@@ -1,9 +1,27 @@
-;; -*- lexical-binding: t -*-
+;;; tramp-hlo.el --- High level operations as Tramp handlers -*- lexical-binding: t -*-
 
-;;; tramp-hlo.el --- High level operations as Tramp handlers
+;; Copyright (C) 2025 Free Software Foundation, Inc.
+
 ;; Author Joe Sadusk <joe@sadusk.com>
 ;; Version 0.0.1
+;; Package: tramp-hlo
 ;; Package-Requires: ((tramp "2.8.1"))
+;; URL: https://github.com/jsadusk/tramp-hlo
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary
 ;; This is an attempt to optimize Tramp remote editing with slow
